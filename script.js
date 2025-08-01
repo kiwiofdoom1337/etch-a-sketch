607f989df252da40a8e14aed9e156485d2fb9b73
kiwiofdoom1337 <186161940+kiwiofdoom1337@users.noreply.github.com>
2025-08-01 14:23:28 +0300

    add input validation, button style, change initial grid load code

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ resetBtn.classList.add("reset-btn");
 resetBtn.textContent = "Reset Squares";
 document.body.prepend(resetBtn);
 
+let compStyles = getComputedStyle(container);
+let contWidth = compStyles.width;
+let contHeight = compStyles.height;
+
 for (let i = 0; i < 256; i++) {
   let gridElement = document.createElement("div");
   gridElement.classList.add("element");
+  console.log(contWidth);
+
+  gridElement.style.width = `${
+    contWidth.slice(0, contWidth.length - 2) / 16
+  }px`;
+
+  gridElement.style.height = `${
+    contHeight.slice(0, contHeight.length - 2) / 16
+  }px`;
 
   container.appendChild(gridElement);
 }
@@ -14,16 +27,21 @@ for (let i = 0; i < 256; i++) {
 resetBtn.setAttribute("onclick", "resetSquares()");
 
 function resetSquares() {
-  let amount = +prompt("How many squares do you want per side?", "16");
-  if (amount >= 100) {
+  let amount = prompt("How many squares do you want per side?", "16");
+
+  if (amount == null) {
+    return alert("Cancelled.");
+  }
+
+  if (isNaN(+amount)) {
+    return alert("Please enter a valid number under 100.");
+  }
+
+  if (+amount >= 100) {
     return alert(
       "Too many squares! Please select less than 100 squares per side."
     );
   }
-  let compStyles = getComputedStyle(container);
-  let contWidth = compStyles.width;
-  let contHeight = compStyles.height;
-
   container.innerHTML = "";
 
   for (let i = 0; i < amount * amount; i++) {
